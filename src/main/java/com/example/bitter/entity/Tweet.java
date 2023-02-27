2221f6ed5454c691987126f0850f271d4860c745
Ren <ren.tran@protonmail.com>
2023-02-27 10:14:37 -0800

    implemented tweet entity

---
+++ src/main/java/com/example/bitter/entity/Tweet.java
@@ -0,0 +1,39 @@
+package com.example.bitter.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Tweet {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private Timestamp posted; // must be generated upon creation
+
+    private boolean deleted;
+
+    private String content;
+
+    // TODO: add User and Hashtag imports
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "tweet_id")
+    private Tweet inReplyTo;
+
+    @ManyToOne
+    @JoinColumn(name = "tweet_id")
+    private Tweet repostOf;
+
+    @OneToMany(mappedBy = "tweet")
+    private Hashtag hashtag;
+}
