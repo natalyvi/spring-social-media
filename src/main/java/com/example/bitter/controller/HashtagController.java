f99eeee27bc3471b03ed6d720557b5711346396e
Shawn Zhou <shawnzhou10@hotmail.com>
2023-02-28 10:15:34 -0600

    Fixed mapping path

--- src/main/java/com/example/bitter/controller/HashtagController.java
+++ src/main/java/com/example/bitter/controller/HashtagController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/hashtag")
+@RequestMapping("/tag")
 public class HashtagController {
     private final HashtagService hashtagService;
 }
