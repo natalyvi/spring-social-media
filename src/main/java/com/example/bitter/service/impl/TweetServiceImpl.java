3cb257473b37a1093ee38c3ff585535f12a34305
Ren <ren.tran@protonmail.com>
2023-02-28 07:41:16 -0800

    added annotations and fields to tweetcontroller, tweetserviceimpl

--- src/main/java/com/example/bitter/service/impl/TweetServiceImpl.java
+++ src/main/java/com/example/bitter/service/impl/TweetServiceImpl.java
@@ -1,4 +1,15 @@
 package com.example.bitter.service.impl;
 
-public class TweetServiceImpl {
+import com.example.bitter.mapper.TweetMapper;
+import com.example.bitter.repository.TweetRepository;
+import com.example.bitter.service.TweetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TweetServiceImpl implements TweetService {
+
+    private final TweetRepository tweetRepository;
+    private final TweetMapper tweetMapper;
 }
