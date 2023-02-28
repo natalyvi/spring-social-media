410ddd506a5dd9906b362bf10f8b0970ad00a298
Ren <ren.tran@protonmail.com>
2023-02-28 08:05:28 -0800

    implemented tweetcontroller advice

--- src/main/java/com/example/bitter/controller/advice/TweetControllerAdvice.java
+++ src/main/java/com/example/bitter/controller/advice/TweetControllerAdvice.java
@@ -1,5 +1,21 @@
 package com.example.bitter.controller.advice;
 
+import com.example.bitter.dto.ErrorDto;
+import com.example.bitter.exception.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice(basePackages = {"com.example.bitter.controllers"})
+@ResponseBody
 public class TweetControllerAdvice {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+   public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
+       return new ErrorDto(badRequestException.getMessage());
+   }
 }
