7f822c4ac7bb8b4d56a091116c91ab0fb55468ac
Shawn Zhou <shawnzhou10@hotmail.com>
2023-02-28 10:26:20 -0600

    Created validate controller/service classes

---
+++ src/main/java/com/example/bitter/service/ValidateService.java
@@ -0,0 +1,5 @@
+package com.example.bitter.service;
+
+public interface ValidateService {
+    
+}
---
+++ src/main/java/com/example/bitter/service/impl/ValidateServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.bitter.service.impl;
+
+import com.example.bitter.service.ValidateService;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ValidateServiceImpl implements ValidateService {
+    
+}
