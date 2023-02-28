1f3052673018821397ccf866e0ff0972c647ce82
Shawn Zhou <shawnzhou10@hotmail.com>
2023-02-28 10:16:00 -0600

    Fixed mapping

--- src/main/java/com/example/bitter/controller/HashtagController.java
+++ src/main/java/com/example/bitter/controller/HashtagController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/tag")
+@RequestMapping("/tags")
 public class HashtagController {
     private final HashtagService hashtagService;
 }
