190bb9be01274b10e4d5a2fb3d9efc3bd449d5cf
Nataly Villalba <alexandra191192@hotmail.com>
2023-02-27 22:09:51 -0500

    Create UserRepository Interface #33

---
+++ src/main/java/com/example/bitter/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.bitter.repository;
+
+import com.example.bitter.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
