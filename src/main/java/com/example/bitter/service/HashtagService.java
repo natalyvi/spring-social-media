7e2f49c852637a1f9d86d9fe929bfa8d2a2cc079
Shawn Zhou <shawnzhou10@hotmail.com>
2023-03-01 10:43:36 -0600

    Implemented get all tags endpoint

--- src/main/java/com/example/bitter/service/HashtagService.java
+++ src/main/java/com/example/bitter/service/HashtagService.java
@@ -1,5 +1,14 @@
 package com.example.bitter.service;
 
+import java.util.List;
+
+import com.example.bitter.dto.HashtagDto;
+import com.example.bitter.dto.TweetResponseDto;
+
 public interface HashtagService {
+
+    List<HashtagDto> getAllTags();
+
+    List<TweetResponseDto> getAllTweetsWithTag(String label);
     
 }
--- src/main/java/com/example/bitter/service/impl/HashtagServiceImpl.java
+++ src/main/java/com/example/bitter/service/impl/HashtagServiceImpl.java
@@ -4,9 +4,13 @@ import com.example.bitter.service.HashtagService;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.bitter.repository.HashtagRepository;
+import com.example.bitter.dto.HashtagDto;
+import com.example.bitter.dto.TweetResponseDto;
 import com.example.bitter.mapper.HashtagMapper;
 @Service
 @RequiredArgsConstructor
@@ -14,5 +18,15 @@ public class HashtagServiceImpl implements HashtagService{
     
     private final HashtagRepository hashtagRepository;
     private final HashtagMapper hashtagMapper;
+    
+    @Override
+    public List<HashtagDto> getAllTags() {
+        return hashtagMapper.entitiesToDto(hashtagRepository.findAll());
+    }
+    @Override
+    public List<TweetResponseDto> getAllTweetsWithTag(String label) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllTweetsWithTag'");
+    }
 
 }
