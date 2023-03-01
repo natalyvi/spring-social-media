5290058d6724bc51e8721bad282e660880faa7c2
Ren <ren.tran@protonmail.com>
2023-03-01 15:45:48 -0800

    reattempting fix hashtagrepo

--- src/main/java/com/example/bitter/repository/HashtagRepository.java
+++ src/main/java/com/example/bitter/repository/HashtagRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HashtagRepository extends JpaRepository <Hashtag, Integer>{
+public interface HashtagRepository extends JpaRepository <Hashtag, Long>{
     Optional<Hashtag> findByLabel(String label);
 }
