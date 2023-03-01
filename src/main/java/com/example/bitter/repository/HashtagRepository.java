81b0a1ba94e8e037854d3a9a709d6610ca69458c
Ren <ren.tran@protonmail.com>
2023-03-01 15:42:59 -0800

    attempting to fix hashtagrepo merge conflict

--- src/main/java/com/example/bitter/repository/HashtagRepository.java
+++ src/main/java/com/example/bitter/repository/HashtagRepository.java
@@ -4,7 +4,9 @@ import com.example.bitter.entity.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HashtagRepository extends JpaRepository <Hashtag, Long>{
-    
+    Optional<Hashtag> findByLabel(String label);
 }
