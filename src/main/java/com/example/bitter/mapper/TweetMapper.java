8fa536965eba463b9d503e516dc4930d39a1aedf
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-03-01 09:32:11 -0600

    Update TweetMapper.java

--- src/main/java/com/example/bitter/mapper/TweetMapper.java
+++ src/main/java/com/example/bitter/mapper/TweetMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.Mapper;
 import java.util.List;
 import java.util.Set;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = { UserMapper.class })
 public interface TweetMapper {
     TweetResponseDto entityToDto(Tweet entity);
 
