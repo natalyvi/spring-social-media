a16ba1c48fe8a591984ca3bb0c009e213ea9d6e7
Shawn Zhou <shawnzhou10@hotmail.com>
2023-03-01 16:55:16 -0600

    Implemented validate endpoints

--- src/main/java/com/example/bitter/controller/ValidateController.java
+++ src/main/java/com/example/bitter/controller/ValidateController.java
@@ -1,6 +1,8 @@
 package com.example.bitter.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,19 @@ import com.example.bitter.service.ValidateService;
 @RequestMapping("/validate")
 public class ValidateController {
     private final ValidateService validateService;
+
+    @RequestMapping("/tag/exists/{label}")
+    public Boolean hashtagExists(@PathVariable String label) {
+        return validateService.tagExists(label);
+    }
+
+    @RequestMapping("/username/exists/@{username}") 
+    public Boolean usernameExists(@PathVariable String username) {
+        return validateService.usernameExists(username);
+    }
+
+    @RequestMapping("/username/available/@{username}")
+    public Boolean usernameAvailable(@PathVariable String username) {
+        return validateService.usernameAvailable(username);
+    }
 }
